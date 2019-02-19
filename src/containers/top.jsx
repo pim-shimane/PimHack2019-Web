3364b5f286e0a315a701a99fe220d80193dc8743
t-kazu <s153057@matsu.shimane-u.ac.jp>
2019-02-19 12:20:13 +0900

    componentsとcontainersを追加しました

---
+++ src/containers/top.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// ここにcomponentをインポート
+import Hello from '../components/hello.jsx'
+
+class TopScreen extends Component {
+  render() {
+    return (
+      <div className="main-padding-menu">
+        <Hello />
+      </div>
+    );
+  }
+}
+
+export default TopScreen;
