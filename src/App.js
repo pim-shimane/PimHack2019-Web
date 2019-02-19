832bcb0f621408f8b879c587a82d87e152497226
t-kazu <s153057@matsu.shimane-u.ac.jp>
2019-02-19 16:53:26 +0900

    redux導入しました

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Top from './containers/top.jsx';
 
 class App extends Component {
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
           <Top />
+          <h5>{this.state}</h5>
         </header>
       </div>
     );
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import credit from './creditReducer.js';
+
+const reducer = combineReducers({
+    credit
+});
+
+export default reducer;
