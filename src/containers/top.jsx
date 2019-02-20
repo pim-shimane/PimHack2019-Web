416c4853c691da39a8c2a58fbe2225f54670616c
Sugitaku <s173046@matsu.shimane-u.ac.jp>
2019-02-20 11:05:07 +0900

    コンフリクト解消

--- src/containers/top.jsx
+++ src/containers/top.jsx
@@ -1,29 +1,18 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 
 // ここにcomponentをインポート
 import Hello from '../components/hello.jsx'
 import Upload from '../components/upload.jsx'
 
 class TopScreen extends Component {
-  constructor (props) {
-    super(props);
-  }
   render() {
     return (
       <div className="main-padding-menu">
         <Hello />
-        {this.props.num}
-        {this.props.english}
         <Upload />
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  num: state.lesson.num,
-  english: state.courseCredit.english
-});
-
-export default connect(mapStateToProps)(TopScreen);
+export default TopScreen;
