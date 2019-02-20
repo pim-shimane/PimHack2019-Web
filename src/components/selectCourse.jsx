7b2eb7fe9417f6353434b985f252ddcbf9b287b1
t-kazu <s153057@matsu.shimane-u.ac.jp>
2019-02-20 11:58:28 +0900

    学科をconstにしました

--- src/components/selectCourse.jsx
+++ src/components/selectCourse.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdbreact";
 
+import { informationSystem } from "../reducers/courseReducer.js"; 
+import { informationEngineering } from "../reducers/courseReducer.js"; 
+import { inteligenceInformationDesign } from "../reducers/courseReducer.js"; 
+import { machineElectronics } from "../reducers/courseReducer.js"; 
+
 const SelectCourse = () => {
     return (
       <MDBDropdown>
@@ -8,14 +13,15 @@ const SelectCourse = () => {
           学科
         </MDBDropdownToggle>
         <MDBDropdownMenu basic>
-          <MDBDropdownItem>数理・情報システム学科（数理構造コース、数理解析コース）</MDBDropdownItem>
-          <MDBDropdownItem>数理・情報システム学科（情報システムコース）</MDBDropdownItem>
-          <MDBDropdownItem>数理・情報システム学科（情報工学コース）</MDBDropdownItem>
-          <MDBDropdownItem>知能情報デザイン学科</MDBDropdownItem>
-        </MDBDropdownMenu>
+          <MDBDropdownItem>{informationSystem}</MDBDropdownItem>
+          <MDBDropdownItem>{informationEngineering}</MDBDropdownItem>
+          <MDBDropdownItem>{inteligenceInformationDesign}</MDBDropdownItem>
+          <MDBDropdownItem>{machineElectronics}</MDBDropdownItem>
+       </MDBDropdownMenu>
       </MDBDropdown>
     );
   }
   
 
 export default SelectCourse;
+
---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,45 @@
+const informationSystem = "数理・情報システム学科（情報システムコース）";
+const informationEngineering = "数理・情報システム学科（情報工学コース）";
+const inteligenceInformationDesign = "知能情報デザイン学科";
+const machineElectronics = "機械・電気電子工学科";
+
+const initialState = {
+  course: 0, //英語
+  foreignLanguage: 0, //初修
+  artCulture: 0, //芸術文化
+  informationScience: 0, //情報科学
+  socialElements: 0, //入門人文社会科学
+  scienceElements: 0, //入門自然科学
+  interdisciplinaryElements: 0, //入門学際
+  socialAdvance: 0, //発展人文社会科学
+  scienceAdvance: 0, //発展自然科学
+  interdisciplinaryAdvance: 0, //発展学際
+  socialTrain: 0, //社会人力養成
+  freeFirst: 0, //自由選択Ⅰ
+  specialFundamental: 0, //専門基礎
+  specialCompulsory: 0, //専門必修
+  specialOptional: 0, //専門選択
+  specialFree: 0, //専門自由
+  freeSecond: 0, //自由選択Ⅱ
+  surplusCredit: 0 //余剰単位
+};
+// Reducer処理
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "Sample": {
+      //後で変えて下さい。
+      return state;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export {
+  reducer as default,
+  informationSystem,
+  informationEngineering,
+  inteligenceInformationDesign,
+  machineElectronics
+};
