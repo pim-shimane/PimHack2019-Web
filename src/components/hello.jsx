aa28c0666b9b9bf26c1d1fb4dd9f75d8b3967f34
t-kazu <s153057@matsu.shimane-u.ac.jp>
2019-02-20 10:15:07 +0900

    学科別履修単位数をjsonに書きました

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,13 +1,26 @@
+// 実験用のファイルです。最終的に削除します。
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+// import informationSyatemCredit from "../courses/informationSystem";
 
 class HelloScreen extends Component {
+  constructor (props) {
+    super(props);
+  }
   render() {
     return (
       <div>
         <h5>Hello</h5>
+        <h5>{this.props.num}</h5>
+        <h5>{this.props.english}</h5>
       </div>
     );
   }
 }
+const mapStateToProps = state => ({
+  num: state.lesson.num,
+  english: state.courseCredit.english
+});
 
-export default HelloScreen;
+export default connect(mapStateToProps)(HelloScreen);
--- src/containers/top.jsx
+++ src/containers/top.jsx
@@ -1,27 +1,16 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 
 // ここにcomponentをインポート
 import Hello from '../components/hello.jsx'
 
 class TopScreen extends Component {
-  constructor (props) {
-    super(props);
-  }
   render() {
     return (
       <div className="main-padding-menu">
         <Hello />
-        {this.props.num}
-        {this.props.english}
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  num: state.lesson.num,
-  english: state.courseCredit.english
-});
-
-export default connect(mapStateToProps)(TopScreen);
+export default TopScreen;
