3c98015a4bcb2bd260c09766e02c1ed5bb541f6d
t-kazu <s153057@matsu.shimane-u.ac.jp>
2019-02-20 16:00:51 +0900

    lessonReducerを削除しました

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import lesson from "./lessonReducer.js";
 import courseCredit from "./courseCreditReducer.js";
 import course from "./courseReducer.js";
 
 const reducer = combineReducers({
-  lesson,
   course,
   courseCredit
 });
