de8be73bb6e22f13f88f8e33f422bbac8a7c5a4d
Hashitani Yasuhiro <hashitani6@outlook.jp>
2019-02-21 11:23:24 +0900

    デザインの修正しました

--- src/components/ifDone.jsx
+++ src/components/ifDone.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class IfDone extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="container done-space">
         <div className="row">
           <div className="col-md-12">
             <h1 className="display-3  text-center">あと？単位足りません。</h1>
--- src/components/warning.jsx
+++ src/components/warning.jsx
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 class Warning extends Component {
   render() {
     return (
+      <div>
         <p id="attention">このアプリを使って問題が起きてもpimは一切保証いたしません。</p>
+        <p>また、アップロードしたデータにつきましてはサーバーに保存されることなく処理されています。</p>
+      </div>
     );
   }
 }
