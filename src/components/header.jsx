0311c744cd57f736ab2d9eed0e3ed4a801641e4a
ymkz <ymkzly@gmail.com>
2019-02-21 20:10:32 +0900

    chore: remove unused files and move static files

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import Icon from '../img/pimlogo.png';
+
 class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-light mb-3">
         <div className="container">
-          <a href="https://pim-shimane.com/"><img src={Icon} height="40px" alt=""></img></a>
+          <a href="https://pim-shimane.com/">
+            <img src="/images/pimlogo.png" height="40px" alt="" />
+          </a>
         </div>
       </nav>
     );
