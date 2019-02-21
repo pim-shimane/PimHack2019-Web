12ce9fe0580f35e8b936aae9a5d1fadb9b4110e2
Hashitani Yasuhiro <hashitani6@outlook.jp>
2019-02-22 01:41:13 +0900

    授業科目一覧のデザインを作成しました

--- src/components/evaluationSheet.jsx
+++ src/components/evaluationSheet.jsx
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { MDBBtn, MDBCollapse, MDBContainer, MDBCard, MDBCardBody, MDBIcon, MDBListGroup, MDBListGroupItem} from "mdbreact";
+
 
 class Sheet extends Component {
+  state = {
+    collapseID: ""
+  }
+
+  toggleCollapse = collapseID => () => {
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+    }));
+  }
+
   render() {
     return (
       <div>
@@ -18,70 +30,94 @@ class Sheet extends Component {
               <div className="mx-auto">
                 <table>
                   <thead>
-                  <tr>
-                    <th></th>
-                    <th>英語</th>
-                    <th>初修</th>
-                    <th>健康・スポーツ/文化・芸術</th>
-                    <th>情報科学</th>
-                    <th>人文社会科学</th>
-                    <th>自然科学</th>
-                    <th>その他の教養育成</th>
-                    <th>自由選択１</th>
-                    <th>専門基礎</th>
-                    <th>専門必修</th>
-                    <th>専門選択</th>
-                    <th>専門自由</th>
-                    <th>自由選択２</th>
-                    <th>余剰単位</th>
-                  </tr>
+                    <tr>
+                      <th></th>
+                      <th>英語</th>
+                      <th>初修</th>
+                      <th>健康・スポーツ/文化・芸術</th>
+                      <th>情報科学</th>
+                      <th>人文社会科学</th>
+                      <th>自然科学</th>
+                      <th>その他の教養育成</th>
+                      <th>自由選択１</th>
+                      <th>専門基礎</th>
+                      <th>専門必修</th>
+                      <th>専門選択</th>
+                      <th>専門自由</th>
+                      <th>自由選択２</th>
+                      <th>余剰単位</th>
+                    </tr>
                   </thead>
                   <tbody>
-                  <tr>
-                    <th>必要</th>
-                    <td>{this.props.needcredit.english}</td>
-                    <td>{this.props.needcredit.foreignLanguage}</td>
-                    <td>{this.props.needcredit.artCulture}</td>
-                    <td>{this.props.needcredit.informationScience}</td>
-                    <td>{this.props.needcredit.social}</td>
-                    <td>{this.props.needcredit.science}</td>
-                    <td>{this.props.needcredit.educationOthers}</td>
-                    <td>{this.props.needcredit.freeFirst}</td>
-                    <td>{this.props.needcredit.specialFundamental}</td>
-                    <td>{this.props.needcredit.specialCompulsory}</td>
-                    <td>{this.props.needcredit.specialOptional}</td>
-                    <td>{this.props.needcredit.specialFree}</td>
-                    <td>{this.props.needcredit.freeSecond}</td>
-                    <td>-</td>
-                  </tr>
+                    <tr>
+                      <th>必要</th>
+                      <td>{this.props.needcredit.english}</td>
+                      <td>{this.props.needcredit.foreignLanguage}</td>
+                      <td>{this.props.needcredit.artCulture}</td>
+                      <td>{this.props.needcredit.informationScience}</td>
+                      <td>{this.props.needcredit.social}</td>
+                      <td>{this.props.needcredit.science}</td>
+                      <td>{this.props.needcredit.educationOthers}</td>
+                      <td>{this.props.needcredit.freeFirst}</td>
+                      <td>{this.props.needcredit.specialFundamental}</td>
+                      <td>{this.props.needcredit.specialCompulsory}</td>
+                      <td>{this.props.needcredit.specialOptional}</td>
+                      <td>{this.props.needcredit.specialFree}</td>
+                      <td>{this.props.needcredit.freeSecond}</td>
+                      <td>-</td>
+                    </tr>
                   </tbody>
                   <tbody>
-                  <tr>
-                    <th>取得</th>
-                    <td>{this.props.credit.english}</td>
-                    <td>{this.props.credit.foreignLanguage}</td>
-                    <td>{this.props.credit.artCulture}</td>
-                    <td>{this.props.credit.informationScience}</td>
-                    <td>{this.props.credit.social}</td>
-                    <td>{this.props.credit.science}</td>
-                    <td>{this.props.credit.educationOthers}</td>
-                    <td>{this.props.needcredit.freeFirst}</td>
-                    <td>{this.props.credit.specialFundamental}</td>
-                    <td>{this.props.credit.specialCompulsory}</td>
-                    <td>{this.props.credit.specialOptional}</td>
-                    <td>{this.props.credit.specialFree}</td>
-                    <td>{this.props.credit.freeSecond}</td>
-                    <td>{this.props.credit.surplusCredit}</td>
-                  </tr>
+                    <tr>
+                      <th>取得</th>
+                      <td>
+                        <a color="primary" onClick={this.toggleCollapse("basicCollapse")} style={{ marginBottom: "1rem" }}>
+                          {this.props.credit.english}
+                          <MDBIcon icon="caret-down"/>
+                        </a>
+                      </td>
+                      <td>
+                        <a color="primary" onClick={this.toggleCollapse("basicCollapse")} style={{ marginBottom: "1rem" }}>
+                          {this.props.credit.foreignLanguage}
+                          <MDBIcon icon="caret-down"/>
+                        </a>
+                      </td>
+                      <td>{this.props.credit.artCulture}</td>
+                      <td>{this.props.credit.informationScience}</td>
+                      <td>{this.props.credit.social}</td>
+                      <td>{this.props.credit.science}</td>
+                      <td>{this.props.credit.educationOthers}</td>
+                      <td>{this.props.needcredit.freeFirst}</td>
+                      <td>{this.props.credit.specialFundamental}</td>
+                      <td>{this.props.credit.specialCompulsory}</td>
+                      <td>{this.props.credit.specialOptional}</td>
+                      <td>{this.props.credit.specialFree}</td>
+                      <td>{this.props.credit.freeSecond}</td>
+                      <td>{this.props.credit.surplusCredit}</td>
+                    </tr>
                   </tbody>
                 </table>
+                <MDBCollapse id="basicCollapse" isOpen={this.state.collapseID}>
+                  <MDBContainer>
+                    <MDBCard>
+                      <MDBListGroup>
+                        <MDBListGroupItem>hoge</MDBListGroupItem>
+                        <MDBListGroupItem>hoge</MDBListGroupItem>
+                      </MDBListGroup>
+                    </MDBCard>
+                  </MDBContainer>
+              </MDBCollapse>
               </div>
             </div>
+          </div>
         </div>
       </div>
-    </div>
     );
   }
+}
+
+class CollapsePage extends Component {
+
 }
 
 // export default Sheet;
