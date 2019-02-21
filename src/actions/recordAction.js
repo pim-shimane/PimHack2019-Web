969201dec53b85cf04a5a119929d8e62c61ee36e
t-kazu <s153057@matsu.shimane-u.ac.jp>
2019-02-21 12:29:25 +0900

    console.logを削除しました

--- src/actions/recordAction.js
+++ src/actions/recordAction.js
@@ -1,7 +1,6 @@
 export const REGISTER_RECORD = "REGISTER_RECORD";
 
 export function registerRecords(records) {
-  console.log(records);
   return {
     type: REGISTER_RECORD,
     records: records
