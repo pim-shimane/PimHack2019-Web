9b1dbe2eb406016601fb888e36b814125a321c00
奥田蓮 <sprict@okudarennoMacBook-puro.local>
2019-02-22 12:01:12 +0900

    数理構造コース・数理解析コースの専門必修を配列として追加しました

---
+++ src/courses/mathematics.js
@@ -0,0 +1,46 @@
+export const mathematics = "数理・情報システム学科（数理構造コース, 数理解析コース）";
+
+export default {
+  english: 4, //英語
+  foreignLanguage: 4, //初修
+  artCulture: 2, //芸術文化
+  informationScience: 2, //情報科学
+  social: 4, //人文社会科学
+  science: 4, //自然科学
+  educationOthers: 6, //教養育成その他
+  freeFirst: 10, //自由選択Ⅰ
+  specialFundamental: 12, //専門基礎
+  specialCompulsory: 34, //専門必修
+  specialOptional: 28, //専門選択
+  specialFree: 8, //専門自由
+  freeSecond: 6 //自由選択Ⅱ
+};
+
+export const mathematicsRequired = [
+  "代数学入門Ｉ",
+  "代数学入門II",
+  "幾何学入門ＩＡ",
+  "幾何学入門IIＢ",
+  "位相幾何学入門Ｉ",
+  "位相幾何学入門II",
+  "数理構造演習セミナーＩＡ",
+  "数理構造演習セミナーＩＢ",
+  "数理構造演習セミナーIIＡ",
+  "数理構造演習セミナーIIＢ",
+  "解析学入門ＩＡ",
+  "解析学入門IIＡ",
+  "数理解析演習セミナーＩＡ",
+  "数理解析演習セミナーＩＢ",
+  "数理解析演習セミナーIIＡ",
+  "数理解析演習セミナーIIＢ",
+  "計算数学Ｉ",
+  "計算数学II",
+  "数学要論ＩＡ",
+  "数学要論ＩＢ",
+  "数学要論IIＡ",
+  "数学要論IIＢ",
+  "線形代数学",
+  "基礎解析学Ｉ",
+  "基礎解析学II",
+  "卒業研究"
+];
